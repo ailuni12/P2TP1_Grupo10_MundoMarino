59c2161f5ae9801926db67f4c78500ce2dceee8b
ailuni12 <ailunicliz@gmail.com>
2025-06-28 22:34:30 -0300

    .

--- src/gestormundomarino/Zona.java
+++ src/gestormundomarino/Zona.java
@@ -1,5 +1,16 @@
 package gestormundomarino;
 
 public class Zona {
+    public int idZona;
+    public String nombre;
+
+    public Zona(int idZona, String nombre) {
+        this.idZona = idZona;
+        this.nombre = nombre;
+    }
+
+    public Zona() {
+    }
+    
     
 }
